e74f942ebbd86acf08988991ab08862c4384dbc2
powerstephen <power.stephen@gmail.com>
2025-11-25 17:40:55 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import EdgeTierRoiSimple from "@/components/EdgeTierRoiSimple";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-100 py-10">
+      <EdgeTierRoiSimple />
+    </main>
+  );
+}
